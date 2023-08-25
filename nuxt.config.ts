2f4290d00449354e9a75b451b507f024225e6541
gabrielstuff <gabriel@soixantecircuits.fr>
2023-08-26 01:08:07 +0200

    ⚙️ conf: change . to www default site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   site: {
-    url: 'https://charts.tools',
+    url: 'https://www.charts.tools',
     name: 'Charts tools',
   },
   devtools: { enabled: true },
